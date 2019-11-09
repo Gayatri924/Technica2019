fd84e34e107fa28dd48fe80eb7e5a3e918012e3e
Gayatri Gopavajhala <gayatri924@gmail.com>
2019-11-09 13:34:47 -0500

    gameUpdates

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
-//Page is --> http://127.0.0.1:8080/
-
-// -------------- load packages -------------- //
-// INITIALIZATION STUFF
-
-var express = require('express')
+var express = require('express');
 var app = express();
 var path = require('path');
 var request = require('request');
 var hbs = require('hbs');
-
-// -------------- express initialization -------------- //
-// PORT SETUP - NUMBER SPECIFIC TO THIS SYSTEM
+var fs = require('fs');
+var cookieSession = require('cookie-session');
 
 app.set('port', process.env.PORT || 8080 );
 app.set('view engine', 'hbs');
 
-//link files
+app.use(express.static('client'));
 
-app.use('/css', express.static(path.join(__dirname, 'css')));
-app.use('/js', express.static(path.join(__dirname, 'js')));
-
-// -------------- express 'get' handlers -------------- //
-// These 'getters' are what fetch your pages
-
-app.get('/', function(req, res){
-    res.send('Homepage');
-});
-
-
-// -------------- listener -------------- //
-// // The listener is what keeps node 'alive.' 
+var controllers = require('./controllers');
+controllers.set(app);
 
 var listener = app.listen(app.get('port'), function() {
   console.log( 'Express server started on port: '+listener.address().port );
